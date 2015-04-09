ce1cc7c5c9c17fcb4e4bf9c41e1b7682587667ef
Pierre Marijon <pierre.marijon@inria.fr>
2015-04-09 13:46:22 +0200

    MTG addd abstract class for observe FindBreakpoints

---
+++ include/IFindObserver.hpp
@@ -0,0 +1,46 @@
+/*****************************************************************************
+ *   MindTheGap: Integrated detection and assembly of insertion variants
+ *   A tool from the GATB (Genome Assembly Tool Box)
+ *   Copyright (C) 2014  INRIA
+ *   Authors: C.Lemaitre, G.Rizk
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#ifndef _TOOL_IFindObserver_HPP_
+#define _TOOL_IFindObserver_HPP_
+
+/********************************************************************************/
+
+
+/********************************************************************************/
+
+template<size_t span>
+class FindBreakpoints;
+
+template<size_t span>
+class IFindObserver
+{
+public:
+    
+    IFindObserver(FindBreakpoints<span>* find);
+
+    virtual void update() = 0;
+
+protected :
+
+    FindBreakpoints<span>* _find;
+};
+
+#endif /* _TOOL_IFindObserver_HPP_ */
