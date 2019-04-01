daa48309a6067c3b0e85925435265db1501077de
cervin.guyomar <cervin.guyomar@gmail.com>
2019-04-01 16:14:25 +0200

    More verbose simplification script

--- pipeline/genome_graph/graph_simplification.py
+++ pipeline/genome_graph/graph_simplification.py
@@ -13,16 +13,20 @@ op.add_argument("infile")
 op.add_argument("outfile")
 opts = op.parse_args()
 
+print("Loading graph")
 g = genome_graph.GenomeGraph.read_gfa(opts.infile) 
 
-
+print("Popping bubbles")
 g.pop_all_bubbles()
 
+print("Merging linear paths")
 g.merge_all_linear_paths()
 
+print("Merging redundancies")
 g.merge_all_gapfillings()
 
 # rerun linear path
+print("Merging linear paths")
 g.merge_all_linear_paths()
 
 g.write_gfa(opts.outfile)
